e5352359623604dcda0b624990af4433e13d5530
lovechinowu <lovechinowu@tencent.com>
2022-05-08 11:58:49 +0800

    update

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+let name = "yxc";
+let age = 18;
+
+function print() {
+    console.log("My name is " + name);
+}
+
+export {
+    name, print
+}
---
+++ static/js/index2.js
@@ -0,0 +1,9 @@
+let x = 3;
+
+function print_x() {
+    console.log("function print_x: " + x);
+}
+
+export {
+    x, print_x
+}
